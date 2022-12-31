e068255542ab1725e17742c274b2b5a50fd2dc3d
LHX090126 <99321020+LHX090126@users.noreply.github.com>
2022-12-31 13:56:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '8.136.215.190'
+      url.hostname = 'twitter.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
